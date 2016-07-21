a2eee34b43e4ecab457f243564b7f55d9f9915ab
Maxim Usolcev <babulyakarate@gamil.com>
2016-07-21 18:16:07 +0600

    Create new method Petooh File --> Token Array

--- src/main/kotlin/PetoohAndToken.kt
+++ src/main/kotlin/PetoohAndToken.kt
@@ -1,14 +1,79 @@
+import java.io.File
+import java.util.*
+
 /**
  * Created by Vedrovski on 21.07.2016.
  */
 class PetoohAndToken {
 
-    fun getFile(){
 
+    fun setFileToToken(fileKoko: String): Array<Token> { //Petooh -> Token
+
+        val strToToken = File(fileKoko).readText()//string with Koko
+        val tokenArray = ArrayList<Token>()//return
+
+        for (index in 0..strToToken.length) {
+
+            if (strToToken.startsWith("ko", index) == true) {
+
+                tokenArray.add(Token.PLUS)
+                continue
+            }
+
+            if (strToToken.startsWith("Ko", index) == true) {
+
+                tokenArray.add(Token.MINUS)
+                continue
+
+            }
+
+            if (strToToken.startsWith("kud", index) == true) {
+
+                tokenArray.add(Token.END)
+                continue
+            }
+
+            if (strToToken.startsWith("Kud", index) == true) {
+
+                tokenArray.add(Token.BEGIN)
+                continue
+            }
+
+            if (strToToken.startsWith("kudah", index) == true) {
+                tokenArray.add(Token.LEFT)
+                continue
+            }
+
+            if (strToToken.startsWith("Kudah", index) == true) {
+
+                tokenArray.add(Token.RIGHT)
+                continue
+            }
+
+            if (strToToken.startsWith("kukarek", index) == true) {
+
+                tokenArray.add(Token.WRITE)
+                continue
+            }
+
+            if (strToToken.startsWith("Kukarek", index) == true) {
+
+                tokenArray.add(Token.READ)
+                continue
+            }
+
+
+        }
+
+        val retArray:Array<Token> = tokenArray.toTypedArray()
+
+        return retArray
     }
 
-    fun setFile(){
-        
+
+    fun setFileToKoko(arrToken: Array<Token>, output: String) { // Token -> Petooh
+
+
     }
 
 }
