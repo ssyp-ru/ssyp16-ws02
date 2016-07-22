b02200595da709652d0d05a476441aa115facbfb
Kirill Fedoseev <fedoskir@mail.ru>
2016-07-22 06:16:18 -0700

    Add tokenCompiler tests

--- src/test/kotlin/TokenCompilerTest.kt
+++ src/test/kotlin/TokenCompilerTest.kt
@@ -10,4 +10,13 @@ class TokenCompilerTest {
         testClass(arrayOf(Token.READ, Token.PLUS, Token.WRITE), "5", "6")
         testClass(arrayOf(Token.READ, Token.PLUS, Token.PLUS, Token.WRITE), "5", "7")
     }
+
+    @Test fun testMinus(){
+        testClass(arrayOf(Token.READ, Token.MINUS, Token.WRITE), "5", "4")
+        testClass(arrayOf(Token.READ, Token.MINUS, Token.MINUS, Token.WRITE), "5", "3")
+    }
+
+    @Test fun testLeftRight(){
+        testClass(arrayOf(Token.READ, Token.PLUS, Token.RIGHT, Token.READ, Token.PLUS, Token.WRITE, Token.LEFT, Token.WRITE), "58", "96")
+    }
 }
