7754e77d1aba8de1e8e4e48ea431502d8ce58132
Maxim Usolcev <babulyakarate@gamil.com>
2016-07-22 14:36:08 +0600

    Refractoring code

--- src/test/kotlin/TestFileToToken.kt
+++ src/test/kotlin/TestFileToToken.kt
@@ -16,16 +16,12 @@ open class TestFileToToken {
      */
     @Test fun testFileToToken() {
         val newFile = File(fileKo).writeText("Kk koKuduhkoKokuKud ko KO 099991d dsv shdb Kudah kukarek ku ko KOd Ko")
-
         val arrToken = testObj.setFileToToken(fileKo)
-
         var bool = false
-
         if (arrToken is Array<Token>) {
             bool = true
         }
         assertEquals(true, bool)
-
     }
 
     /**
@@ -33,7 +29,6 @@ open class TestFileToToken {
     This test use setFileTiToken and call ecs:NotFoundFile , check answer on empty array
      */
     @Test fun returnNotFoundFile() {
-
         val arrToken = testObj.setFileToToken("NotFile.not")
         assertEquals(true,arrToken.isEmpty())
     }
@@ -43,25 +38,16 @@ open class TestFileToToken {
     This test use setFileToKoko with NORMAL parameters(FILE FOUND) and check answer on type(Array<Token>)
      */
     @Test fun testFileToKoko() {
-
         val testObj = PetoohTranslater()
-
         val newFile = File(fileKo).writeText("Kk koKuduhkoKokuKud ko KO 099991d dsv shdb Kudah kukarek ku ko KOd Ko")
         val arrToken = testObj.setFileToToken(fileKo)
-
         testObj.setFileToKoko(arrToken,fileOut)
-
         val AnotherarrToken = testObj.setFileToToken(fileKo)
-
         var bool = false
-
         if (AnotherarrToken is Array<Token>) {
             bool = true
         }
-
         assertEquals(true, bool)
-
-
     }
 
     /**
@@ -69,16 +55,9 @@ open class TestFileToToken {
     This test use setFileToKoko with empty array and check output file on empty or not empty
      */
     @Test fun emptyArrayTokens(){
-
         val arrEmpty = emptyArray<Token>()
-
         testObj.setFileToKoko(arrEmpty,"output2.txt")
-
         val retStr = File("output2.txt").readText()
-
         assertEquals("",retStr)
-
     }
-
-
 }
