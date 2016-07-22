96bd85539126e32b4186905ca5500d37f3d4d9fd
Maxim Usolcev <babulyakarate@gamil.com>
2016-07-22 14:28:55 +0600

    Thats all

---
+++ src/main/kotlin/PetoohTranslater.kt
@@ -0,0 +1,76 @@
+import sun.invoke.empty.Empty
+import java.io.File
+import java.io.FileNotFoundException
+import java.util.*
+
+/**
+ Class PetoohTranslater have two method: setFileToToken and setFileToKoko
+ */
+
+class PetoohTranslater {
+
+    /**
+    setFileToToken - method which take file with Petooh code and return array with Tokens
+    If file not founded, returned emtpy array type Token
+    @param fileKoko name of file
+    @returns array of Tokens
+     */
+
+    fun setFileToToken(fileKoko: String): Array<Token> { //Petooh -> Token
+        val strWithTokens: String
+        try {                                                             ////
+            strWithTokens = File(fileKoko).readText()//string with Koko  ////
+        } catch (exc: FileNotFoundException) {                          ////       catch(exc: FileNotFoundException)
+            println("File not found!")                                   ////
+            return emptyArray()                                         ////
+        }                                                               ///
+        val tokenArray = ArrayList<Token>()//return
+        for (index in 0..strWithTokens.length) {
+
+            when {
+                strWithTokens.startsWith("ko", index) -> tokenArray.add(Token.PLUS)
+                strWithTokens.startsWith("Ko", index) -> tokenArray.add(Token.MINUS)
+                strWithTokens.startsWith("kud", index) -> tokenArray.add(Token.END)
+                strWithTokens.startsWith("Kud", index) -> tokenArray.add(Token.BEGIN)
+                strWithTokens.startsWith("kudah", index) -> tokenArray.add(Token.LEFT)
+                strWithTokens.startsWith("Kudah", index) -> tokenArray.add(Token.RIGHT)
+                strWithTokens.startsWith("kukarek", index) -> tokenArray.add(Token.WRITE)
+                strWithTokens.startsWith("Kukarek", index) -> tokenArray.add(Token.READ)
+            }
+        }
+
+        val retArray: Array<Token> = tokenArray.toTypedArray()
+
+        return retArray//arr
+    }
+
+    /**
+     setFileToKoko - method which take array with Tokens and write in output file
+     If file not found , created new file (mention)
+     @param arrToken:Array<Token> array of Token
+     @param output:String name of output file
+     @returns file with Petooh code
+     */
+
+    fun setFileToKoko(arrToken: Array<Token>, output: String) { // Token -> Petooh
+        val endFile = File(output)//output File
+        var timeStr = StringBuilder()
+
+        for (index in 0 until arrToken.size) {
+            when (arrToken[index]) {
+                Token.PLUS -> timeStr.append("ko")
+                Token.MINUS -> timeStr.append("Ko")
+                Token.READ -> timeStr.append("Kukarek")
+                Token.END -> timeStr.append("kud")
+                Token.BEGIN -> timeStr.append("Kud")
+                Token.LEFT -> timeStr.append("kudah")
+                Token.RIGHT -> timeStr.append("Kudah")
+                Token.WRITE -> timeStr.append("kukarek")
+
+            }
+        }
+        val endStr = timeStr.toString()
+        endFile.appendText(endStr)// write if output file
+    }
+
+}
--- src/test/kotlin/TestFileToToken.kt
+++ src/test/kotlin/TestFileToToken.kt
@@ -1,22 +1,82 @@
 import org.junit.Test
+import java.io.File
+import java.io.FileNotFoundException
 import kotlin.test.assertEquals
-
+import kotlin.test.assertFailsWith
+//!!!!!!!!!!!! DELETE FILE.TXT,NEXT TEST
 
 open class TestFileToToken {
+    val testObj = PetoohTranslater()
+    val fileKo = "Petooh.txt"
+    val fileOut = "output.txt"
 
-    @Test fun testAllMethod() {
+    /**
+     TEST METHOD: setFileToToken
+     This test use setFileToToken with NORMAL parameters(FILE FOUND) and check answer on type(Array<Token>)
+     */
+    @Test fun testFileToToken() {
+        val newFile = File(fileKo).writeText("Kk koKuduhkoKokuKud ko KO 099991d dsv shdb Kudah kukarek ku ko KOd Ko")
 
-        val testObj = PetoohAndToken()
-        val arrToken = testObj.setFileToToken("Petooh.txt")
+        val arrToken = testObj.setFileToToken(fileKo)
 
         var bool = false
 
         if (arrToken is Array<Token>) {
             bool = true
         }
+        assertEquals(true, bool)
+
+    }
+
+    /**
+    TEST METHOD: setFileToToken
+    This test use setFileTiToken and call ecs:NotFoundFile , check answer on empty array
+     */
+    @Test fun returnNotFoundFile() {
+
+        val arrToken = testObj.setFileToToken("NotFile.not")
+        assertEquals(true,arrToken.isEmpty())
+    }
+
+    /**
+    TEST METHOD: setFileToKoko
+    This test use setFileToKoko with NORMAL parameters(FILE FOUND) and check answer on type(Array<Token>)
+     */
+    @Test fun testFileToKoko() {
+
+        val testObj = PetoohTranslater()
+
+        val newFile = File(fileKo).writeText("Kk koKuduhkoKokuKud ko KO 099991d dsv shdb Kudah kukarek ku ko KOd Ko")
+        val arrToken = testObj.setFileToToken(fileKo)
+
+        testObj.setFileToKoko(arrToken,fileOut)
+
+        val AnotherarrToken = testObj.setFileToToken(fileKo)
+
+        var bool = false
+
+        if (AnotherarrToken is Array<Token>) {
+            bool = true
+        }
 
         assertEquals(true, bool)
 
+
+    }
+
+    /**
+    TEST METHOD: setFileToKoko
+    This test use setFileToKoko with empty array and check output file on empty or not empty
+     */
+    @Test fun emptyArrayTokens(){
+
+        val arrEmpty = emptyArray<Token>()
+
+        testObj.setFileToKoko(arrEmpty,"output2.txt")
+
+        val retStr = File("output2.txt").readText()
+
+        assertEquals("",retStr)
 
     }
 
