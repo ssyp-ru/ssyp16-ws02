7cb82509eb93f6fbe4c84035e4535a533cb5c098
Maxim Usolcev <babulyakarate@gamil.com>
2016-07-23 12:09:36 +0600

    Main conflict

---
+++ src/main/kotlin/CLI.kt
@@ -0,0 +1,14 @@
+/**
+ * Created by Vedrovski on 22.07.2016.
+ */
+class CLI {
+
+    fun MainMenu(){
+        println("PetooKahn version 1.0.1 .")
+        println("--> 1.Run Petooh or BF file")
+        println("--> 2.Petooh translate to BF")
+        println("--> 3.BF translate to Petooh")
+        println("--> 4.Compile BF or Petooh file")
+
+    }
+}
