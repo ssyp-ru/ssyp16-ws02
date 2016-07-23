8639ead26341294375b7270ff9c6531e8b82b260
Koval Dan <kovaldan@yandex.ru>
2016-07-22 23:38:55 -0700

    for maxim

--- src/main/kotlin/Interpreter.kt
+++ src/main/kotlin/Interpreter.kt
@@ -1,72 +1,86 @@
 import java.io.InputStream
 import java.io.PrintStream
 
-class Interpreter(var arrayToken: Array<Token>, val read: InputStream, val write: PrintStream) {
+class Interpreter(val read: InputStream = System.`in`, val write: PrintStream = System.out) {
     private val const = 30000
     private var array = ByteArray (const, { 0 })
     private var current = 0
     private var i = 0
     private var count = i
 
+
     /**
      * This fun interprets tokens to Kotlin.
      */
 
-    fun interpret() {
+    fun interpret(arrayToken: Array<Token>) {
         while (i != arrayToken.size) {
             when (arrayToken[i]) {
-                Token.LEFT -> {
-                    if (current != 0) {
-                        current--
-                    } else
-                        current = const - 1
-                }
-
-                Token.RIGHT -> {
-                    if (current != const - 1) {
-                        current++
-                    } else
-                        current = 0
-                }
-
+                Token.LEFT -> left()
+                Token.RIGHT -> right()
                 Token.MINUS -> array[current]--
-
-
                 Token.PLUS -> array[current]++
-
-
                 Token.READ -> array[current] = read.read().toByte()
-
-
                 Token.WRITE -> write.print(array[current].toChar())
-
-
-                Token.BEGIN -> {
-                    if (array[current].toInt() == 0) {
-                        while (arrayToken[i] != Token.END) {
-                            i++
-                        }
-                        i++
-                    }
-                }
-                Token.END -> {
-                    if (array[current].toInt() != 0) {
-                        while (!((arrayToken[i] == Token.BEGIN) && (count == 0))) {
-                            if (arrayToken[i] == Token.END) {
-                                count++
-                            }
-                            if ((arrayToken[i] == Token.BEGIN) && (count > 0)) {
-                                count--
-                            }
-                            if ((arrayToken[i] == Token.BEGIN) && (count == 0)) {
-                                break
-                            }
-                            i--
-                        }
-                    }
-                }
+                Token.BEGIN -> begin(arrayToken)
+                Token.END -> end(arrayToken)
             }
             i++
+        }
+    }
+
+    /**
+     * Moves cursor left.
+     */
+
+    fun left() {
+        if (current != 0) {
+            current--
+        } else
+            current = const - 1
+    }
+
+    /**
+     * Moves cursor right
+     */
+
+    fun right() {
+        if (current != const - 1) {
+            current++
+        } else
+            current = 0
+    }
+
+    /**
+     * Starts loop if array[current] = 0
+     */
+
+    private fun begin(arrayToken: Array<Token>) {
+        if (array[current].toInt() == 0) {
+            while (arrayToken[i] != Token.END) i++
+            i++
+        }
+    }
+
+    /**
+     * Ends loop if array[current] = 0
+     */
+
+    private fun end(arrayToken: Array<Token>) {
+        if (array[current].toInt() != 0) {
+            while (!((arrayToken[i] == Token.BEGIN) && (count == 0))) {
+                if (arrayToken[i] == Token.END) {
+                    count++
+                }
+                if ((arrayToken[i] == Token.BEGIN) && (count > 0)) {
+                    count--
+                }
+                if ((arrayToken[i] == Token.BEGIN) && (count == 0)) {
+                    break
+                }
+                i--
+
+            }
         }
     }
 }
