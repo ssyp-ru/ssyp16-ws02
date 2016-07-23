b4c54c7799fd2f4096e2094a2b7b67b36cbac054
Kirill Fedoseev <fedoskir@mail.ru>
2016-07-23 12:19:23 +0500

    add full loops and checking difficult tests with using tokenizer

--- src/test/kotlin/TokenCompilerTest.kt
+++ src/test/kotlin/TokenCompilerTest.kt
@@ -1,25 +1,26 @@
 import org.junit.Test
+import java.io.File
 
 class TokenCompilerTest {
 
     /**
      * Tests simple read and write
      */
-    @Test fun testReadWrite(){
+    @Test fun testReadWrite() {
         testClass(arrayOf(Token.READ, Token.WRITE), "5", "5")
     }
 
     /**
      * Tests reading twice
      */
-    @Test fun testReadTwice(){
+    @Test fun testReadTwice() {
         testClass(arrayOf(Token.READ, Token.READ, Token.WRITE), "dk", "k")
     }
 
     /**
      * Tests plus
      */
-    @Test fun testPlus(){
+    @Test fun testPlus() {
         testClass(arrayOf(Token.READ, Token.PLUS, Token.WRITE), "5", "6")
         testClass(arrayOf(Token.READ, Token.PLUS, Token.PLUS, Token.WRITE), "5", "7")
     }
@@ -27,7 +28,7 @@ class TokenCompilerTest {
     /**
      * Tests minus
      */
-    @Test fun testMinus(){
+    @Test fun testMinus() {
         testClass(arrayOf(Token.READ, Token.MINUS, Token.WRITE), "5", "4")
         testClass(arrayOf(Token.READ, Token.MINUS, Token.MINUS, Token.WRITE), "5", "3")
     }
@@ -35,14 +36,36 @@ class TokenCompilerTest {
     /**
      * Tests moving cursor left and right
      */
-    @Test fun testLeftRight(){
+    @Test fun testLeftRight() {
         testClass(arrayOf(Token.READ, Token.PLUS, Token.RIGHT, Token.READ, Token.PLUS, Token.WRITE, Token.LEFT, Token.WRITE), "f8", "9g")
     }
 
     /**
      * Tests writing twice
      */
-    @Test fun testWriteTwice(){
+    @Test fun testWriteTwice() {
         testClass(arrayOf(Token.READ, Token.WRITE, Token.WRITE), "g", "gg")
+    }
+
+    @Test fun testToZero() {
+        testClass(arrayOf(Token.READ, Token.BEGIN, Token.MINUS, Token.END, Token.WRITE), "8", 0.toChar().toString())
+    }
+
+    @Test fun testFactorial() {
+        val testFile = File("test.txt")
+        testFile.writeText(",>>>>+<<<<[->+[->+>+<<]>[-<+>]>[->[->+>+<<]>[-<+>]<<]>[-]>>[-<<+>>]<<<<<<]>>>>.")
+        testClass(BrainfuckTranslator().translateToTokens("test.txt"), 5.toChar().toString(), 120.toChar().toString())
+    }
+
+    @Test fun testHelloWorld() {
+        val testFile = File("test.txt")
+        testFile.writeText("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.")
+        testClass(BrainfuckTranslator().translateToTokens("test.txt"), "", "Hello World!\n")
+    }
+
+    @Test fun testDiv() {
+        val testFile = File("test.txt")
+        testFile.writeText("+>,+>,<[<+>>[<<->>->+>]>[<<+>>[<+>-]]<<<<[<]>>-]<-.")
+        testClass(BrainfuckTranslator().translateToTokens("test.txt"), 6.toChar().toString() + 2.toChar().toString(), 3.toChar().toString())
     }
 }
