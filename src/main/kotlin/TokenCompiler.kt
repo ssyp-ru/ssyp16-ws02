aeae774e314caa64cc0ddc4591d762de3d26a553
Kirill Fedoseev <fedoskir@mail.ru>
2016-07-27 10:31:35 +0600

    fix warnings

--- src/main/kotlin/TokenCompiler.kt
+++ src/main/kotlin/TokenCompiler.kt
@@ -1,8 +1,6 @@
-import jdk.internal.org.objectweb.asm.util.ASMifier
 import org.objectweb.asm.ClassWriter
 import org.objectweb.asm.Label
 import org.objectweb.asm.MethodVisitor
-import org.objectweb.asm.Opcodes
 import org.objectweb.asm.Opcodes.*
 import java.io.File
 import java.util.*
