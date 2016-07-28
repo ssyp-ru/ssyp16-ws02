efe3b5b92448efe63cab0d8fae7f2bb9c79ab48f
Kirill Fedoseev <fedoskir@mail.ru>
2016-07-28 21:02:18 +0600

    add ParserValidator, fix main

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>){
     val compiler = TokenCompiler()
-    if(args.size != 2)
+    if(args.size == 2)
         compiler.compile(args[0], args[1])
     else
         println("Bad arguments")
